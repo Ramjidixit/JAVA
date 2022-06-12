44b04b2180d4be0d308348e2ca9395f8191ab8f9
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-06-12 21:14:15 +0530

    Add files via upload

---
+++ Basic input output/first progarm in java.java
@@ -0,0 +1,8 @@
+//FIRST PROGRAM IN JAVA
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("JAI MAA GANGE\nJAI BABA BHOLENATH\nJAI SHREE KRISHNA\nFIRST PROGRAM IN JAVA BY RAM");
+	}
+}
