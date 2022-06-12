bb23a1f60446c9b0be1359fff708d901c21cbbbc
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-06-12 22:24:36 +0530

    Add files via upload

---
+++ Basic input output/Write a program to find square of a number..java
@@ -0,0 +1,14 @@
+// Write a program to find square of a number.
+
+import java.util.*;
+public class Main
+
+{
+	public static void main(String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int a=sc.nextInt();
+		System.out.print("The square of the number is:"+(a*a));
+	}
+}
