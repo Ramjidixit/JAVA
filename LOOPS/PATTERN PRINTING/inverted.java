cc0b9f67d6536905b04dda003ce81efff51053a5
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-06-30 23:05:53 +0530

    Add files via upload

---
+++ LOOPS/PATTERN PRINTING/inverted.java
@@ -0,0 +1,21 @@
+/**print the pattern of inverted half pyramid
+        ****
+        ***
+        **
+        *                                       */
+
+import java.util.*;
+public class inverted {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of rows:");
+        int a=sc.nextInt();
+        for(int i=a;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
