1f8532c0bd3aefbe6b18cd85745cf7a019e55c6c
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-07-01 23:45:32 +0530

    Add files via upload

---
+++ LOOPS/PATTERN PRINTING/shreya.java
@@ -0,0 +1,23 @@
+/*print the following pattern
+1
+22
+333
+4444
+5555           */
+
+import java.util.*;
+public class shreya{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of rows:");
+        int a=sc.nextInt();
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=i;j++){
+               
+                System.out.print(i+" ");
+            }
+        System.out.println();
+        }
+    }
+    
+}
