7898f112bc0f440343f67a6abc5e18e5366f7af2
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-07-03 18:59:01 +0530

    Update floyd.java

--- LOOPS/PATTERN PRINTING/floyd.java
+++ LOOPS/PATTERN PRINTING/floyd.java
@@ -1,9 +1,9 @@
 /* print the pattern of Floyd's triangle
- 1
- 2 3
- 4 5 6
- 7 8 9 10
- 11 12 13 14 15             */
+  1
+  2 3
+  4 5 6
+  7 8 9 10
+  11 12 13 14 15             */
 
 import java.util.*;
 public class floyd {
