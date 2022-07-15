63fcb632da437801cc64d3625d025426ed3d2e79
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-07-15 10:38:14 +0530

    Update pattern1.java

--- LOOPS/PATTERN PRINTING/PATTERN1/pattern1.java
+++ LOOPS/PATTERN PRINTING/PATTERN1/pattern1.java
@@ -1,4 +1,4 @@
-/*print the following star pattern for n number of rows 
+/*print the following star pattern for m number of rows 
  *****
  *****
  *****
@@ -7,8 +7,8 @@
 
 public class pattern1 {
     public static void main(String[] args){
-        int n=5;
         int m=4;
+        int n=5;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 System.out.print("*");
