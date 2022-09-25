443b83977d49f36ab4c3dfbfa28ec9de68f7d632
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-09-25 23:51:12 +0530

    Update var_new.java

--- DATA TYPE/var_new.java
+++ DATA TYPE/var_new.java
@@ -2,10 +2,10 @@
 class var_new{
 	int a=10;
 	public static void main(String[] args){
-		//new keyword 
+		//new keyword--CREATE NEW OBJECT AND ALLOCATE DYNAMIC MEMORY AT RUN TIME
 	var_new n=new var_new();
 	System.out.println(n.a);
-	//var keyword
+	//var keyword--TO DECLARE ANY TYPE OF VARIABLE
 	var b=9.8;
 	var c=5;
 	System.out.println(b+c);   
