cf9206d6ede6fdf8e8c5d403171921defee444cd
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-09-25 23:49:32 +0530

    Add files via upload

---
+++ DATA TYPE/var_new.java
@@ -0,0 +1,16 @@
+
+class var_new{
+	int a=10;
+	public static void main(String[] args){
+		//new keyword 
+	var_new n=new var_new();
+	System.out.println(n.a);
+	//var keyword
+	var b=9.8;
+	var c=5;
+	System.out.println(b+c);   
+	
+	
+	}
+}
+
