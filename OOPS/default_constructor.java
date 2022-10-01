55ec60a571dde637cafd4abc324a117f2c5aced7
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-10-01 13:20:15 +0530

    Add files via upload

---
+++ OOPS/default_constructor.java
@@ -0,0 +1,15 @@
+//default constructor 
+
+public class default_constructor{
+	default_constructor(){
+		System.out.println("I am default constructor");
+	}
+	public static void main(String[] args){
+		System.out.println("Hello world");
+		default_constructor obj=new default_constructor();
+		System.out.println("hi world");
+	}
+}
+/*  Hello world
+I am default constructor
+hi world*/
