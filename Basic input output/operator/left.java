fb0e88ff79b2da743172bc28ee23d9985048d51a
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-10-13 13:34:16 +0530

    Add files via upload

---
+++ Basic input output/operator/left.java
@@ -0,0 +1,14 @@
+//left shift and right shift
+
+public class left{
+	public static void main(String[] args){
+		//java leftshift operator
+		System.out.println(10<<3);  //10*2^3=10*8=80
+		System.out.println(10<<2);  //40
+		System.out.println(15<<3);  //120
+		//java rightshift operator
+		System.out.println(10>>2); // 10/2^2=10/4=2  
+		System.out.println(20>>2); // 20/2^2=20/4=5  
+		System.out.println(20>>3); // 20/2^3=20/8=2  
+	}
+}
