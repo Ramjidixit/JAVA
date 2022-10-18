f6b20a445b19a755bafd921ac6fb08d226796879
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-10-18 16:35:29 +0530

    Update Cl1.java

--- class_object_method/Cl1.java
+++ class_object_method/Cl1.java
@@ -4,8 +4,11 @@ number of object using hashcode() */
 //creating class and object
 import java.util.*;
 class Person{
+	//properties-variables
 	String name;
 	int age;
+	
+	//actions-method
 	void talk(){
 	System.out.println("Hello I am "+name);
 	System.out.println("My age is "+age);
@@ -13,8 +16,8 @@ class Person{
 }
 class Cl1{
 	public static void main(String[] args){
-		Person Raju=new Person(); //creating raju object 
-		System.out.println("Hash code="+Raju.hashCode());
+		Person Raju=new Person(); //creating Person class object:Raju 
+		System.out.println("Hash code="+Raju.hashCode()); //finding hashcode
 		Raju.name="Rahim";
 		Raju.age=45;
 		Raju.talk();  //call the talk() method
