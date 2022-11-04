7ddef0bb5911338d7ee1cec63f3cae19b1fa6f44
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-11-04 17:48:00 +0530

    Add files via upload

---
+++ class_object_method/Cl5.java
@@ -0,0 +1,10 @@
+class Cl5// class Name
+{
+    int x = 10;// state(instance variable)
+
+    public static void main(String[] args)// Behaviour(method)
+    {
+        Cl5 t = new Cl5();// object creation
+        System.out.println(t.x);
+    }
+}
