5665a09d64354d5968c2de1890543e4c3b8a76e9
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-11-10 15:10:48 +0530

    Add files via upload

---
+++ Fileh24.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.FileInputStream;
+import java.io.*;
+
+public class Fileh24 {
+    public static void main(String[] args) throws IOException {
+        Console c = System.console();
+        String s = c.readLine();
+        // char[] pass = c.readPassword();
+        System.out.println(s);
+        // FileInputStream f=new FileInputStream(System.in);
+        // BufferedReader br=new BufferedReader(f);
+        // String s2=br.readLine();
+    }
+}
